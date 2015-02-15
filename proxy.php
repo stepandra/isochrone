a9793161e194d6d40db8cb5fc9fe21739062766a
Stefan Mytchyk <stepandra94@gmail.com>
2015-02-15 15:49:50 +0200

    Refactor code
    Changed tab to space, deleted POI.

--- proxy.php
+++ proxy.php
@@ -4,19 +4,19 @@ $valid_url_regex = '/.*/';
 $enable_jsonp    = false;
 $enable_native   = false;
 if ( !$url ) {
-  
+
 
   $contents = 'ERROR: url not specified';
   $status = array( 'http_code' => 'ERROR' );
-  
+
 }  else {
   $ch = curl_init( $url );
-  
+
   if ( strtolower($_SERVER['REQUEST_METHOD']) == 'post' ) {
     curl_setopt( $ch, CURLOPT_POST, true );
     curl_setopt( $ch, CURLOPT_POSTFIELDS, $_POST );
   }
-  
+
   if ( $_GET['send_cookies'] ) {
     $cookie = array();
     foreach ( $_COOKIE as $key => $value ) {
@@ -26,20 +26,20 @@ if ( !$url ) {
       $cookie[] = SID;
     }
     $cookie = implode( '; ', $cookie );
-    
+
     curl_setopt( $ch, CURLOPT_COOKIE, $cookie );
   }
-  
+
   curl_setopt( $ch, CURLOPT_FOLLOWLOCATION, true );
   curl_setopt( $ch, CURLOPT_HEADER, true );
   curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-  
+
   curl_setopt( $ch, CURLOPT_USERAGENT, $_GET['user_agent'] ? $_GET['user_agent'] : $_SERVER['HTTP_USER_AGENT'] );
-  
+
   list( $header, $contents ) = preg_split( '/([\r\n][\r\n])\\1/', curl_exec( $ch ), 2 );
-  
+
   $status = curl_getinfo( $ch );
-  
+
   curl_close( $ch );
 }
 
@@ -51,25 +51,25 @@ if ( $_GET['mode'] == 'native' ) {
     $contents = 'ERROR: invalid mode';
     $status = array( 'http_code' => 'ERROR' );
   }
-  
+
 
   foreach ( $header_text as $header ) {
     if ( preg_match( '/^(?:Content-Type|Content-Language|Set-Cookie):/i', $header ) ) {
       header( $header );
     }
   }
-  
+
   print $contents;
-  
+
 } else {
-  
- 
+
+
   $data = array();
-  
+
 
   if ( $_GET['full_headers'] ) {
     $data['headers'] = array();
-    
+
     foreach ( $header_text as $header ) {
       preg_match( '/^(.+?):\s+(.*)$/', $header, $matches );
       if ( $matches ) {
@@ -77,7 +77,7 @@ if ( $_GET['mode'] == 'native' ) {
       }
     }
   }
-  
+
 
   if ( $_GET['full_status'] ) {
     $data['status'] = $status;
@@ -85,23 +85,23 @@ if ( $_GET['mode'] == 'native' ) {
     $data['status'] = array();
     $data['status']['http_code'] = $status['http_code'];
   }
-  
+
 
   $decoded_json = json_decode( $contents );
   $data['contents'] = $decoded_json ? $decoded_json : $contents;
-  
+
 
   $is_xhr = strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest';
   header( 'Content-type: application/' . ( $is_xhr ? 'json' : 'x-javascript' ) );
-  
+
 
   $jsonp_callback = $enable_jsonp && isset($_GET['callback']) ? $_GET['callback'] : null;
-  
+
 
   $json = json_encode( $data );
-  
+
   print $jsonp_callback ? "$jsonp_callback($json)" : $json;
-  
+
 }
 
 ?>
